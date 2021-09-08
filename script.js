2b12d86cd1977fd1db94c9a2633f9809150db535
nestor1119 <nestorgfortique@gmail.com>
2021-09-09 01:10:09 +0200

    added search bar delete still doesnt delete and add still doesnt havearg

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 let addbtn= document.getElementById('btn+');
 let list= document.getElementById('list1');
 let inputField= document.getElementById('myInput');
+let dltbtn= document.getElementById('deletebtn');
+let searchField= document.getElementById('searchInput')
 
-addbtn.addEventListener('click', function(){
+console.log(searchField);
+addbtn.addEventListener('click', addcountry);
+//dltbtn.addEventListener('click',deletecountry);
+searchField.addEventListener('keyup',(e) =>{
+  console.log(e.target.value)
+
+});
+
+function addcountry() {
   var listItem = document.createElement("li")
+  const deleteButton = document.createElement("Button");
   listItem.innerText = inputField.value;
   list.appendChild(listItem);
+  listItem.appendChild(deleteButton);
   inputField.value = "";
-})
+  deleteButton.innerText = "delete";
+  deleteButton.id = "deletebtn";
+  //deleteButton.onclick = deletecountry();
+  deleteButton.onclick = console.log("hi");
+}
+
+function deletecountry(){
+
+//list.removeChild(listItem);
+//const something = listItem.parentElement;
+//something.remove();
+}
+
+function isitin(element,searchword){
+element.startsWith(searchword);
+
+}
