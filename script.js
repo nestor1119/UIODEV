82f8253a95d1b0e4ea31a67a231350608156ccf7
nestor1119 <nestorgfortique@gmail.com>
2021-09-09 11:48:07 +0200

    isitin works

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ let addbtn= document.getElementById('btn+');
 let list= document.getElementById('list1');
 let inputField= document.getElementById('myInput');
 let dltbtn= document.getElementById('deletebtn');
-let searchField= document.getElementById('searchInput')
-
+let searchField= document.getElementById('searchInput');
+//let listArr= listarray[0];
+let tet1= isitin('saturday','sat');
+console.log(tet1);
 console.log(searchField);
 addbtn.addEventListener('click', addcountry);
 //dltbtn.addEventListener('click',deletecountry);
@@ -13,15 +15,21 @@ searchField.addEventListener('keyup',(e) =>{
 });
 
 function addcountry() {
+  //create button and li elements
   var listItem = document.createElement("li")
   const deleteButton = document.createElement("Button");
+  //put the text inside the li = what user wrote
   listItem.innerText = inputField.value;
+  //append li to ul and deletebtn to li
   list.appendChild(listItem);
   listItem.appendChild(deleteButton);
+  //reset input box
   inputField.value = "";
+  //add id and content inside delete btn
   deleteButton.innerText = "delete";
   deleteButton.id = "deletebtn";
   //deleteButton.onclick = deletecountry();
+  //test when delete button activates
   deleteButton.onclick = console.log("hi");
 }
 
@@ -33,6 +41,8 @@ function deletecountry(){
 }
 
 function isitin(element,searchword){
-element.startsWith(searchword);
-
+var result= element.startsWith(searchword);
+return result;
 }
+
+function searcharray(){}
