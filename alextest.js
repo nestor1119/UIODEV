7b5de207a833b525534b3f667350938ad4bfde37
nestor1119 <nestorgfortique@gmail.com>
2021-09-13 12:17:02 +0200

    something

---
+++ alextest.js
@@ -0,0 +1,5 @@
+//selectors
+const countryInput = document.querySelector('.countryInput');
+const countryButton = document.querySelector('.countryButton');
+const countryList = document.querySelector('.countryList');
+const searchBox = document.getElementById('searchInput');
